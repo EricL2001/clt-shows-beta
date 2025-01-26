85cab4703f8fbc321b6d94f12002cec6abcb1cb0
Eric Levasseur <eric.levass@gmail.com>
2025-01-26 10:26:30 -0500

    fixed footer and hero padding

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-      <div className="bg-gray-800 text-white py-20">
+      <div className="bg-gray-800 text-white py-8">
         <div className="container mx-auto px-4 text-center">
-          <p className="text-xl mb-1 max-w-2xl mx-auto">
+          <p className="text-sm mb-1 max-w-2xl mx-auto">
           © {new Date().getFullYear()} Records On The Wall • All Rights Reserved
           </p>
         </div>
