7dfac78907a8b7e4cb5f8e981ec9a11bfb53052e
Eric Levasseur <eric.levass@gmail.com>
2025-01-26 10:38:17 -0500

    adjusted container padding

--- components/ui/upcoming-shows.tsx
+++ components/ui/upcoming-shows.tsx
@@ -3,7 +3,7 @@ import ShowCard from "@/components/ui/show-card"
 
 export default function UpcomingShowsSection({ venueName, showData, handleClick }: { venueName: string, showData: Show[], handleClick: (href: string) => void }) {
     return (
-      <div className="py-16 bg-gray-900">
+      <div className="pb-16 pt-8 bg-gray-900">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-12 text-white">Upcoming Shows at {venueName}</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
