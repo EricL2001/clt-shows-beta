a0eac454b62ee269784b5dcf049fb76dbeb86ad0
Eric Levasseur <eric.levass@gmail.com>
2025-03-07 10:15:04 -0500

    updated footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
       <div className="bg-gray-800 text-white py-8">
         <div className="container mx-auto px-4 text-center">
           <p className="text-sm mb-1 max-w-2xl mx-auto">
-          © {new Date().getFullYear()} Records On The Wall
+          Records On The Wall © {new Date().getFullYear()}
           </p>
         </div>
       </div>
